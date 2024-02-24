ce7ea6952fa9aae99ce7dd94a1811e52885e0173
tjanuki <t.januki@gmail.com>
2024-02-24 16:20:15 -0500

    Add liff sandbox commands

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -5,7 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
+        alert("Success! you can do something with LIFF API here.")
+        const context = liff.getContext();
+        console.log(context);
+        alert(context);
         console.log("Success! you can do something with LIFF API here.")
+
+        liff
+            .sendMessages([
+                {
+                    type: "text",
+                    text: "Hello, World!",
+                },
+            ])
+            .then(() => {
+                console.log("message sent");
+            })
+            .catch((err) => {
+                console.log("error", err);
+            });
     })
     .catch((error) => {
         console.log(error)
